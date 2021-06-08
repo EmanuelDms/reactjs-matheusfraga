336d5793e59e4db542996fefd397d6f782e2d2ed
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-06-07 23:40:15 -0300

    feat: trabalhando com states

--- src/App.js
+++ src/App.js
@@ -1,61 +1,64 @@
 import React, { Component } from 'react'
 
-class Sobre extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nome: 'Emanuel',
+      contador: 0
+    }
+
+    // muda o contexto global para contexto da função
+    this.aumentar = this.aumentar.bind(this)
+    this.diminuir = this.diminuir.bind(this)
+  }
+
+  aumentar() {
+    // referencia ao estado da classe
+    let state = this.state
+
+    // tamanho do nome
+    let stateNomeSize = state.nome.length - 1
+    // adiciona exclamação e remove invertida
+    if (state.nome[stateNomeSize] === '¡')
+      state.nome = state.nome.substring(0, stateNomeSize)
+    else state.nome += '!'
+
+    // soma ao contador
+    state.contador += 1
+    this.setState(state)
+  }
+
+  diminuir() {
+    // referencia ao estado da classe
+    let state = this.state
+
+    // tamanho do nome
+    let stateNomeSize = state.nome.length - 1
+    // verificação se existe caractere diferente de '!'
+    if (state.nome[stateNomeSize] === '!')
+      state.nome = state.nome.substring(0, stateNomeSize)
+    // adiciona exclamação invertida
+    else state.nome += '¡'
+
+    // subtrai ao contador
+    state.contador = state.contador -= 1
+    this.setState(state)
+  }
+
   render() {
     return (
       <div>
-        <h2>Olá sou o(a) {this.props.username}</h2>
-        <h3>Cargo: {this.props.job}</h3>
-        <h3>{this.props.age} anos</h3>
+        <h1>Contador</h1>
+        <em>{this.state.nome}</em>
+        <h2>
+          <button onClick={this.diminuir}>-</button>
+          {this.state.contador}
+          <button onClick={this.aumentar}>+</button>
+        </h2>
       </div>
     )
   }
-}
-
-class Equipe extends Component {
-  render() {
-    return (
-      <div>
-        <Sobre
-          username={this.props.nome}
-          job={this.props.cargo}
-          age={this.props.idade}
-        />
-        <Social href={this.props.link} />
-        <hr />
-      </div>
-    )
-  }
-}
-
-class Social extends Component {
-  render() {
-    return (
-      <div>
-        <a href={this.props.href}>Profile</a>
-      </div>
-    )
-  }
-}
-
-const App = () => {
-  return (
-    <div>
-      <h1>Conheça nossa equipe: </h1>
-      <Equipe
-        nome="Emanuel"
-        cargo="Programador Frontend"
-        idade={18}
-        link="https://facebook.com"
-      />
-      <Equipe
-        nome="Moisés"
-        cargo="Programador Backend"
-        idade={19}
-        link="https://linkedin.com"
-      />
-    </div>
-  )
 }
 
 export default App
