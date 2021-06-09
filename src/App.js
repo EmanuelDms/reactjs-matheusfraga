921542da6c1a5e28ba158a258b9d02040072ec0e
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-06-09 11:23:36 -0300

    feat: renderização condicional com states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
-import Membro from './components/Membro'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      status: true
+    }
+    this.sair = this.sair.bind(this)
+    this.entrar = this.entrar.bind(this)
   }
+
+  sair() {
+    this.setState({ status: false })
+  }
+
+  entrar() {
+    this.setState({ status: true })
+  }
+
   render() {
     return (
       <div>
-        <Membro nome="Visitante" />
+        {this.state.status ? (
+          <div>
+            <h1>Bem-vindo ao sistema!</h1>
+            <button onClick={this.sair}>Sair do sistema</button>
+          </div>
+        ) : (
+          <div>
+            <h1>Olá visitante!</h1>
+            <button onClick={this.entrar}>Entrar no sistema</button>
+          </div>
+        )}
       </div>
     )
   }
