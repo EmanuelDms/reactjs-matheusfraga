82ca06e709178a1f87932f39d10c254ab79887ee
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-06-09 09:48:56 -0300

    feat: main lifecycle react

--- src/App.js
+++ src/App.js
@@ -1,61 +1,35 @@
 import React, { Component } from 'react'
 
-class Sobre extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hora: '00:00:00'
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() })
+    }, 1000)
+  }
+
+  componentDidUpdate() {
+    console.log('Atualizou!!')
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log('Vai atualizar')
+  }
+
   render() {
     return (
       <div>
-        <h2>Olá sou o(a) {this.props.username}</h2>
-        <h3>Cargo: {this.props.job}</h3>
-        <h3>{this.props.age} anos</h3>
+        <h1>Hello World!</h1>
+        {this.state.hora}
       </div>
     )
   }
-}
-
-class Equipe extends Component {
-  render() {
-    return (
-      <div>
-        <Sobre
-          username={this.props.nome}
-          job={this.props.cargo}
-          age={this.props.idade}
-        />
-        <Social href={this.props.link} />
-        <hr />
-      </div>
-    )
-  }
-}
-
-class Social extends Component {
-  render() {
-    return (
-      <div>
-        <a href={this.props.href}>Profile</a>
-      </div>
-    )
-  }
-}
-
-const App = () => {
-  return (
-    <div>
-      <h1>Conheça nossa equipe: </h1>
-      <Equipe
-        nome="Emanuel"
-        cargo="Programador Frontend"
-        idade={18}
-        link="https://facebook.com"
-      />
-      <Equipe
-        nome="Moisés"
-        cargo="Programador Backend"
-        idade={19}
-        link="https://linkedin.com"
-      />
-    </div>
-  )
 }
 
 export default App
