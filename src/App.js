0c6274a829d6d044a00cee0fef53734dcca4653b
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-06-16 11:03:54 -0300

    feat: creating basis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import Biscoito from './components/Biscoito'
+import React, { Component } from 'react'
+import pathCronometro from './assets/cronometro.png'
+import './css/style.css'
 
-const App = () => {
-  return <Biscoito />
+export default class App extends Component {
+  render() {
+    return (
+      <div id="container">
+        <div className="item" id="cronometro">
+          <img src={pathCronometro} />
+          <a id="timer">0.0</a>
+        </div>
+        <div className="item" id="areaBtn">
+          <a className="botao">INICIAR</a>
+          <a className="botao">LIMPAR</a>
+          <a className="botao">LIMPAR</a>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App
