09e7e0563072415afc93d2a0057b343a5580e8c2
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-09 18:57:04 -0300

    chore: add semicolon

--- src/components/Imagem/index.js
+++ src/components/Imagem/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import BiscoitoFechado from '../../assets/biscoito.png'
-import BiscoitoAberto from '../../assets/open-biscoito.png'
+import React from 'react';
+import BiscoitoFechado from '../../assets/biscoito.png';
+import BiscoitoAberto from '../../assets/open-biscoito.png';
 
 export default function Imagem(props) {
   return (
     <div>
       <img src={props.attr.textoFrase ? BiscoitoAberto : BiscoitoFechado} />
     </div>
-  )
+  );
 }
--- src/components/biscoito/index.js
+++ src/components/biscoito/index.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react'
-import '../../css/style.css'
-import Imagem from '../Imagem'
-import CTAButton from '../CTAButton'
+import React, { Component } from 'react';
+import '../../css/style.css';
+import Imagem from '../Imagem';
+import CTAButton from '../CTAButton';
 
 export default class Biscoito extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       textoFrase: ''
-    }
+    };
+
     this.frases = [
       'Você cresce mais quando levanta os outros.',
       'Com chuva ou com sol o seu dia será maravilhoso.',
       'A cada nova manhã, nasce junto uma nova chance.',
       'Percebi que minha vida era uma vasta página vazia, e queu podia fazer o que quisesse com ela.',
       'Que gota por gota, nosso coração seja preenchido de paz e felicidade!'
-    ]
+    ];
 
-    this.quebraBiscoito = this.quebraBiscoito.bind(this)
-    this.resetarBiscoito = this.resetarBiscoito.bind(this)
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+    this.resetarBiscoito = this.resetarBiscoito.bind(this);
   }
 
   quebraBiscoito() {
-    let state = this.state
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
-    state.textoFrase = this.frases[numeroAleatorio]
-    this.setState(state)
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = this.frases[numeroAleatorio];
+    this.setState(state);
   }
 
   resetarBiscoito() {
-    return this.setState({ textoFrase: '' })
+    return this.setState({ textoFrase: '' });
   }
 
   render() {
@@ -41,6 +42,6 @@ export default class Biscoito extends Component {
           {this.state.textoFrase && `"${this.state.textoFrase}"`}
         </h3>
       </div>
-    )
+    );
   }
 }
