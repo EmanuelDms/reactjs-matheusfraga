6807728c46ab52e5b5fc5a1588772a78d5dcdb0b
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-15 12:10:43 -0300

    contato screen

---
+++ src/pages/Contato/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Contato() {
+  return (
+    <div>
+      <h1>Contatos</h1> <br />
+      <span>Email: teste@teste.com</span>
+    </div>
+  );
+}
