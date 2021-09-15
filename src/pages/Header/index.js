8edb5d569e01c1fbd038e8159e2bed40ffc7dd56
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-15 12:10:55 -0300

    header links

---
+++ src/pages/Header/index.js
@@ -0,0 +1,21 @@
+import { Grid as GridContainer, styled, useTheme } from "@material-ui/core";
+import React from "react";
+import { Link as Anchor } from "react-router-dom";
+
+export default function Header() {
+  const theme = useTheme();
+
+  const Grid = styled(GridContainer)({
+    border: theme.palette.secondary,
+    width: "100%",
+  });
+  const Link = styled(Anchor)({});
+
+  return (
+    <Grid container justifyContent="space-around">
+      <Link to="/">Home</Link>
+      <Link to="/contato">Contato</Link>
+      <Link to="/sobre">Sobre</Link>
+    </Grid>
+  );
+}
